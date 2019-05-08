9e1746c37ab2dbccff94268f98fece24969eaead
SignpostMarv <me@signpostmarv.name>
2019-05-08 18:57:59 +0100

    increasing test coverage

--- test/TOTPTest.php
+++ test/TOTPTest.php
@@ -128,4 +128,112 @@ class TOPTTest extends TestCase
             );
         }
     }
+
+    /**
+     * @dataProvider dataProviderFailureOfGetCode
+     *
+     * @param array<int, mixed> $constructorArgs
+     *
+     * @psalm-param array{0:int, 1:int, 2:int, 3:string} $constructorArgs
+     * @psalm-param class-string<\Throwable> $expectedException
+     */
+    public function testFailureOfGetCode(
+        array $constructorArgs,
+        string $expectedException,
+        string $expectedExceptionMessage,
+        string $sharedSecret,
+        int $counterValue
+    ) {
+        $totp = $this->getTOTP($constructorArgs);
+
+        $this->assertSame($constructorArgs[2], $totp->getLength());
+        $this->assertSame($constructorArgs[1], $totp->getTimeStep());
+
+        $this->expectException($expectedException);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+
+        $totp->getCode($sharedSecret, $counterValue);
+    }
+
+    /**
+     * @psalm-return Generator<int, array{0:array{0:int, 1:int, 2:int, 3:string}, 1:class-string<\Throwable>, 2:string, 3:string, 4:int}, mixed, void>
+     */
+    public function dataProviderFailureOfGetCode(): \Generator
+    {
+        $seed = Hex::decode(
+            "3132333435363738393031323334353637383930"
+        );
+        $seed32 = Hex::decode(
+            "3132333435363738393031323334353637383930" .
+            "313233343536373839303132"
+        );
+        // Seed for HMAC-SHA512 - 64 bytes
+        $seed64 = Hex::decode(
+            "3132333435363738393031323334353637383930" .
+            "3132333435363738393031323334353637383930" .
+            "3132333435363738393031323334353637383930" .
+            "31323334"
+        );
+
+        $sha1 = [0, 30, 8, 'sha1'];
+        $sha256 = [0, 30, 8, 'sha256'];
+        $sha512 = [0, 30, 8, 'sha512'];
+
+        $times = [
+            59,
+            1111111109,
+            1111111111,
+            1234567890,
+            2000000000,
+        ];
+
+        if (PHP_INT_SIZE > 4) {
+            $times[] = 20000000000;
+        }
+
+        $badLengthArgs = [
+            0,
+            11,
+        ];
+
+        foreach ($times as $time) {
+            foreach ($badLengthArgs as $badLength) {
+                $sha1[2] = $badLength;
+                $sha256[2] = $badLength;
+                $sha512[2] = $badLength;
+
+                yield [
+                    $sha1,
+                    \OutOfRangeException::class,
+                    'Length must be between 1 and 10, as a consequence of RFC 6238.',
+                    $seed,
+                    $time,
+                ];
+                yield [
+                    $sha256,
+                    \OutOfRangeException::class,
+                    'Length must be between 1 and 10, as a consequence of RFC 6238.',
+                    $seed32,
+                    $time,
+                ];
+                yield [
+                    $sha512,
+                    \OutOfRangeException::class,
+                    'Length must be between 1 and 10, as a consequence of RFC 6238.',
+                    $seed64,
+                    $time,
+                ];
+            }
+        }
+    }
+
+    /**
+     * @param array<int, mixed> $constructorArgs
+     *
+     * @psalm-param array{0:int, 1:int, 2:int, 3:string}
+     */
+    protected function getTOTP(array $constructorArgs) : TOTP
+    {
+        return new TOTP(...$constructorArgs);
+    }
 }
